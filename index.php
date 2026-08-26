a4ed5914f2aaf37e75c686ce890239789942ef04
Creativo <anthonylepichon@gmail.com>
2026-08-26 07:55:43 +0100

    Initialise la structure du projet

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Description générale :
+ * Point d'entrée initial de l'application QUIDITMIEUX.
+ *
+ * Rôle :
+ * Réserver l'emplacement du futur point d'entrée et du routeur simple.
+ *
+ * Tâches :
+ * Ne réaliser aucun traitement tant que le socle PHP n'est pas abordé.
+ *
+ * Liens avec les autres fichiers :
+ * Aucun lien actif à cette étape du projet.
+ */
